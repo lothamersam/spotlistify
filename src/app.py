4646b576f987417a600e2f0b6c95c2757e118185
Sam Lothamer <lothamer.sam@gmail.com>
2019-07-12 19:50:17 -0400

    basic deployment files
    add git ignore
    
    add requirments for virtual environment
    
    add profile for heroku
    
    add comments
    
    fix procfile
    
    Add Heroku generated app.json
    
    Fix procfile
    
    fix app.py to support running from gunicorn
    
    fix procfile to look in correct dir
    
    fix procfile to look in correct dir

--- src/app.py
+++ src/app.py
@@ -3,4 +3,8 @@ from main.blueprints import home_blueprint
 
 app = Flask(__name__)
 
+### BLUEPRINTS ###
 app.register_blueprint(home_blueprint.controller)
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True)
--- src/main/blueprints/home_blueprint.py
+++ src/main/blueprints/home_blueprint.py
@@ -1,7 +1,10 @@
 from flask import Blueprint, render_template, abort
 from jinja2 import TemplateNotFound
 
+# define blueprint object for home
 controller = Blueprint('home_blueprint', __name__)
+
+### BLUEPRINT ROUTES ###
 
 @controller.route('/')
 def show():
