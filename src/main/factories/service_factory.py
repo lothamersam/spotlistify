0039f34ffc02c31abc3ceb13c02b1fac365769d6
Sam Lothamer <lothamer.sam@gmail.com>
2019-07-14 21:43:15 -0400

    create service for setlist api

--- src/main/factories/service_factory.py
+++ src/main/factories/service_factory.py
@@ -1,7 +1,14 @@
 from main.services.spotify_service import SpotifyService
+from main.services.setlist_service import SetlistService
 
 class ServiceFactory:
+    spotifyService = SpotifyService()
+    setlistService = SetlistService()
 
     @staticmethod
     def get_spotify_service():
-        return SpotifyService()
+        return ServiceFactory.spotifyService
+
+    @staticmethod
+    def get_setlist_service():
+        return ServiceFactory.setlistService
---
+++ src/main/services/setlist_service.py
@@ -0,0 +1,5 @@
+class SetlistService:
+    
+    @classmethod
+    def search():
+        return ''
