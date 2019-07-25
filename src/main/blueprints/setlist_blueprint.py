7404ac3417b128d8197c64d928b714f68de45d3d
Sam Lothamer <36243786+lothamersam@users.noreply.github.com>
2019-07-25 16:11:02 -0400

    Update setlist_blueprint.py

--- src/main/blueprints/setlist_blueprint.py
+++ src/main/blueprints/setlist_blueprint.py
@@ -3,6 +3,6 @@ from jinja2 import TemplateNotFound
 
 controller = Blueprint('setlist_blueprint', __name__)
 
-@controller.route('/search/{query}')
-def show_index():
+@controller.route('/search/<query>')
+def show_index(query):
     return render_template('home_template.html')
