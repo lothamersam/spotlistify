6fd62b17fda5dadb39ebfbc62c34136d6d96e246
Sam Lothamer <36243786+lothamersam@users.noreply.github.com>
2019-07-25 16:08:53 -0400

    Create setlist_blueprint.py

---
+++ src/main/blueprints/setlist_blueprint.py
@@ -0,0 +1,8 @@
+from flask import Blueprint, render_template, abort
+from jinja2 import TemplateNotFound
+
+controller = Blueprint('setlist_blueprint', __name__)
+
+@controller.route('/search/{query}')
+def show_index():
+    return render_template('home_template.html')
